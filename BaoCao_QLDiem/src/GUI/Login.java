9b5eddff3810e42fafe68b971b782f6329687cf1
tuyen260899 <phamtuyen260899@gmail.com>
2020-12-27 22:05:32 +0700

    Update file Login.java

--- BaoCao_QLDiem/src/GUI/Login.java
+++ BaoCao_QLDiem/src/GUI/Login.java
@@ -24,6 +24,10 @@ public class Login extends javax.swing.JFrame {
         this.setLocationRelativeTo(null);
     }
 
+	public void Test(){
+
+	}
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
